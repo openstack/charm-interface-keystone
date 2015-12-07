fd929c0831b79a14bae5063261f9d8a0fdea83af
Liam Young <liam.young@canonical.com>
2015-12-07 12:59:00 +0000

    gitignore .tox and update to handle all ssl data back from keystone

--- requires.py
+++ requires.py
@@ -21,10 +21,15 @@ class KeystoneRequires(RelationBase):
 
     # These remote data fields will be automatically mapped to accessors
     # with a basic documentation string provided.
+
     auto_accessors = ['private-address', 'service_host', 'service_protocol',
                       'service_port', 'service_tenant', 'service_username',
                       'service_password', 'service_tenant_id', 'auth_host',
-                      'auth_protocol', 'auth_port']
+                      'auth_protocol', 'auth_port', 'admin_token', 'ssl_key',
+                      'ca_cert', 'ssl_cert', 'https_keystone',
+                      'ssl_cert_admin', 'ssl_cert_internal',
+                      'ssl_cert_public', 'ssl_key_admin', 'ssl_key_internal',
+                      'ssl_key_public']
 
     @hook('{requires:keystone}-relation-joined')
     def joined(self):
@@ -33,8 +38,13 @@ class KeystoneRequires(RelationBase):
     def update_state(self):
         if self.base_data_complete():
             self.set_state('{relation_name}.available')
+            if self.ssl_data_complete():
+                self.set_state('{relation_name}.available.ssl')
+            else:
+                self.remove_state('{relation_name}.available.ssl')
         else:
             self.remove_state('{relation_name}.available')
+            self.remove_state('{relation_name}.available.ssl')
 
     @hook('{requires:keystone}-relation-changed')
     def changed(self):
@@ -45,16 +55,44 @@ class KeystoneRequires(RelationBase):
         self.update_state()
 
     def base_data_complete(self):
-        """
-        Get the connection string, if available, or None.
-        """
-        for field in self.auto_accessors:
-            attr_method = getattr(self, field.replace('-', '_'))
-            if not attr_method():
+        data = {
+            'private-address': self.private_address(),
+            'service_host': self.service_host(),
+            'service_protocol': self.service_protocol(),
+            'service_port': self.service_port(),
+            'service_tenant': self.service_tenant(),
+            'service_username': self.service_username(),
+            'service_password': self.service_password(),
+            'service_tenant_id': self.service_tenant_id(),
+            'auth_host': self.auth_host(),
+            'auth_protocol': self.auth_protocol(),
+            'auth_port': self.auth_port(),
+            'admin_token': self.admin_token(),
+        }
+        if all(data.values()):
+            return True
+        return False
+
+    def ssl_data_complete(self):
+        data = {
+            'ssl_key': self.ssl_key(),
+            'ssl_cert': self.ssl_cert(),
+            'ssl_cert_admin': self.ssl_cert_admin(),
+            'ssl_cert_internal': self.ssl_cert_internal(),
+            'ssl_cert_public': self.ssl_cert_public(),
+            'ssl_key_admin': self.ssl_key_admin(),
+            'ssl_key_internal': self.ssl_key_internal(),
+            'ssl_key_public': self.ssl_key_public(),
+            'ca_cert': self.ca_cert(),
+            'https_keystone': self.https_keystone(),
+        }
+        for value in data.values():
+            if not value or value == '__null__':
                 return False
         return True
 
-    def register_endpoints(self, service, region, public_url, internal_url, admin_url):
+    def register_endpoints(self, service, region, public_url, internal_url,
+                           admin_url):
         """
         Register this service with keystone
         """
